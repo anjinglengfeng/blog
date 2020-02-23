1c3779fe5704d1b916f599ba5a74289fd2d8fbdd
anjinglengfeng <286447897@qq.com>
2020-02-23 23:01:57 +0800

    部分网页美化

--- mysite/settings.py
+++ mysite/settings.py
@@ -67,6 +67,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        # 'DIRS': [os.path.join(BASE_DIR,)],
         'DIRS': [os.path.join(BASE_DIR,)] + [os.path.join(BASE_DIR, 'myaccount/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
